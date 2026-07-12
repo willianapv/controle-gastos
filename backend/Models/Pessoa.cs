448cc474e4761cb467d68afd12e21a07a5f5ceec
Willian Almeida de Castro <willian.apv@gmail.com>
2026-07-11 23:29:36 -0300

    feat: configura Entity Framework Core e SQLite

---
+++ backend/Models/Pessoa.cs
@@ -0,0 +1,10 @@
+namespace backend.Models;
+
+public class Pessoa
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = string.Empty;
+
+    public int Idade { get; set; }
+}
